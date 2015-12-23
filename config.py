0a2b1d8be421c30a350f38320601b8eecf89fb43
OSPK <waqas@opensource.com.pk>
2015-12-23 20:05:55 +0500

    2nd

--- config.py
+++ config.py
@@ -6,12 +6,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
 DEBUG = True
 
 HOST = '0.0.0.0'
 
 ADMINS = frozenset(['youremail@yourdomain.con'])
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
